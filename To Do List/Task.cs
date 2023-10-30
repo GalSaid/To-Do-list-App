ca42671d35840a3b606dfd53feb3bca7cd29908b
GalSaid <120250560+GalSaid@users.noreply.github.com>
2023-10-30 11:17:39 +0200

    Add files via upload

---
+++ To Do List/Task.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace To_Do_List
+{
+    public class Task
+    {
+        private string title;
+        private string message;
+
+        public Task(string title, string message)
+        {
+            this.Title = title;
+            this.Message = message;
+        }
+
+        public string Title { get => title; set => title = value; }
+        public string Message { get => message; set => message = value; }
+    }
+}
