d739d0340f6875e420dacf8cbc9ccf43158f4509
GalSaid <120250560+GalSaid@users.noreply.github.com>
2023-11-05 22:36:02 +0200

    Add files via upload

---
+++ To Do List/TaskForm.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace To_Do_List
+{
+    partial class TasksForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.lblMessageLst = new System.Windows.Forms.Label();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.allTasks = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.allTasks)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.lblTitle.Location = new System.Drawing.Point(90, 80);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(50, 22);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Title:";
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.lblMessage.Location = new System.Drawing.Point(47, 139);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(87, 22);
+            this.lblMessage.TabIndex = 1;
+            this.lblMessage.Text = "Message:";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(152, 82);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(180, 22);
+            this.txtTitle.TabIndex = 2;
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(152, 139);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(322, 212);
+            this.txtMessage.TabIndex = 3;
+            // 
+            // lblMessageLst
+            // 
+            this.lblMessageLst.AutoSize = true;
+            this.lblMessageLst.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.lblMessageLst.Location = new System.Drawing.Point(595, 78);
+            this.lblMessageLst.Name = "lblMessageLst";
+            this.lblMessageLst.Size = new System.Drawing.Size(86, 22);
+            this.lblMessageLst.TabIndex = 5;
+            this.lblMessageLst.Text = "Tasks list";
+            // 
+            // btnNew
+            // 
+            this.btnNew.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.btnNew.Location = new System.Drawing.Point(152, 408);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(79, 37);
+            this.btnNew.TabIndex = 6;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.btnSave.Location = new System.Drawing.Point(307, 408);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(83, 37);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.btnRead.Location = new System.Drawing.Point(599, 408);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(76, 37);
+            this.btnRead.TabIndex = 8;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.btnDelete.Location = new System.Drawing.Point(733, 408);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(88, 37);
+            this.btnDelete.TabIndex = 9;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // allTasks
+            // 
+            this.allTasks.AllowUserToAddRows = false;
+            this.allTasks.AllowUserToDeleteRows = false;
+            this.allTasks.AllowUserToResizeColumns = false;
+            this.allTasks.AllowUserToResizeRows = false;
+            this.allTasks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.allTasks.Location = new System.Drawing.Point(599, 118);
+            this.allTasks.Name = "allTasks";
+            this.allTasks.ReadOnly = true;
+            this.allTasks.RowHeadersVisible = false;
+            this.allTasks.RowHeadersWidth = 51;
+            this.allTasks.RowTemplate.Height = 24;
+            this.allTasks.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.allTasks.Size = new System.Drawing.Size(277, 251);
+            this.allTasks.TabIndex = 10;
+            this.allTasks.SelectionChanged += new System.EventHandler(this.allTasks_SelectionChanged);
+            // 
+            // TasksForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(954, 510);
+            this.Controls.Add(this.allTasks);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.lblMessageLst);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.lblTitle);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "TasksForm";
+            this.Text = "To Do list";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.NoteForm_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.allTasks)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblMessage;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label lblMessageLst;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView allTasks;
+    }
+}
+
---
+++ To Do List/TaskForm.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Windows.Forms;
+using System.IO;
+
+namespace To_Do_List
+{
+    public partial class TasksForm : Form
+    {
+        private DataTable table;
+        private int openTaskForEditInd; //if we click the button "read" so now we can also edit the task after this
+        private List<Task> tasksEntries;
+
+        public TasksForm()
+        {
+            InitializeComponent();
+            openTaskForEditInd = -1; //There is not open task to edit
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            table = new DataTable();
+            table.Columns.Add("Title", typeof(string));
+            table.Columns.Add("Message", typeof(string));
+            allTasks.DataSource = table; //connect to table
+            allTasks.Columns["Message"].Visible = false;
+            allTasks.Columns["Title"].Width = 250;
+            LoadDataFromJsonFile();
+        }
+
+        private void btnNew_Click(object sender, EventArgs e) //clean the info
+        {
+            ResetInfo();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e) //save the infromation of the task
+        {
+            if (txtTitle.Text.Trim() == "")
+                MessageBox.Show("You must enter title!");
+            else
+            {
+                if (openTaskForEditInd != -1) //if there is a open note so it will edit the current note
+                {
+                    table.Rows[openTaskForEditInd][0] = txtTitle.Text;
+                    table.Rows[openTaskForEditInd][1] = txtMessage.Text;
+
+                }
+                else //create a new note
+                    table.Rows.Add(txtTitle.Text, txtMessage.Text);
+                ResetInfo();
+            }
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            int rowsCount = allTasks.SelectedRows.Count;
+            if (rowsCount > 1)
+                MessageBox.Show("You can only choose one task!");
+            else if(rowsCount==1)
+            {
+                int index = allTasks.CurrentCell.RowIndex;
+                if (index > -1)
+                {
+                    openTaskForEditInd = index;
+                    txtTitle.Text = table.Rows[index].ItemArray[0].ToString();
+                    txtMessage.Text = table.Rows[index].ItemArray[1].ToString();
+                }
+            }
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            foreach (DataGridViewRow row in allTasks.SelectedRows)
+            {
+                table.Rows[row.Index].Delete();
+            }
+            ResetInfo();
+        }
+
+        private void ResetInfo()
+        {
+            txtTitle.Clear();
+            txtMessage.Clear();
+            openTaskForEditInd = -1; //you cant edit a task until you will press read 
+        }
+
+        private void allTasks_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            openTaskForEditInd = -1;
+        }
+
+        private void NoteForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SaveDataToJsonFile();
+        }
+        private void SaveDataToJsonFile()
+        {
+            tasksEntries = new List<Task>();
+
+            // Retrieve the data from the savedTasksTable and add the task to tasksEntries
+            foreach (DataRow row in table.Rows)
+            {
+                if (!row.IsNull("Title"))
+                {
+                    Task entry = new Task(row["Title"].ToString(), row["Message"].ToString());
+                    tasksEntries.Add(entry);
+                }
+            }
+            // Serialize tasksEntries to JSON
+            string json = JsonConvert.SerializeObject(tasksEntries, Formatting.Indented);
+
+            // Write the JSON data to the file
+            File.WriteAllText("storage.json", json);
+        }
+
+        private void LoadDataFromJsonFile()
+        {
+            string filePath = "storage.json";
+
+            if (!File.Exists(filePath))
+            {
+                // File doesn't exist, create a new one with an empty array
+                File.WriteAllText(filePath, "[]");
+            }
+
+
+            string json = File.ReadAllText("storage.json");
+            tasksEntries = JsonConvert.DeserializeObject<List<Task>>(json);
+
+            // Clear the DataTable
+            table.Rows.Clear();
+
+            // Add the data from noteEntries to the DataTable
+            foreach (Task entry in tasksEntries)
+            {
+                table.Rows.Add(entry.Title, entry.Message);
+            }
+        }
+
+        private void allTasks_SelectionChanged(object sender, EventArgs e)
+        {
+            ResetInfo(); //if we click on onother task it closes the current
+        }
+    }
+}
